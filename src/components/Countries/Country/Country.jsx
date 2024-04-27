1cd5d623ad8abb666013c4cccd67db941ba466f9
nizamuddin0172 <neom01723441081@gmail.com>
2024-04-27 14:36:48 +0600

    codeing wabside ok

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+import CountryDetail from "./CountryDetail/CountryDetail";
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags}) => {
   console.log(country);
   const { name, flags, population, area, cca3} = country;
   const [visited, setVisited] = useState(false);
@@ -9,8 +10,6 @@ const Country = ({ country }) => {
         setVisited(!visited);
   }
 
-
-
   return (
     <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
       <h3 style={{color: visited ? 'purple' : 'white'}}>Name : {name?.common}</h3>
@@ -18,8 +17,20 @@ const Country = ({ country }) => {
       <p>Population : {population}</p>
       <p>area : {area}</p>
       <p><small>Code : {cca3}</small></p>
+      <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+      <br />
+      <br />
+      <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+      <br />
+      <br />
       <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'} </button>
       {visited ? ' i have visited this country' : ' i want to visited'}
+      <hr />
+      <CountryDetail
+      country={country}
+      handleVisitedCountry={handleVisitedCountry}
+      handleVisitedFlags={handleVisitedFlags}
+      ></CountryDetail>
     </div>
   );
 };
---
+++ src/components/Countries/Country/CountryDetail/CountryDetail.jsx
@@ -0,0 +1,18 @@
+import CountryData from "../../../CountryData/CountryData";
+
+const CountryDetail = (props) => {
+    const {country, handleVisitedCountry, handleVisitedFlags} = props;
+    return (
+        <div>
+            <h4>Country Details</h4>
+            <hr />
+            <CountryData
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+            handleVisitedFlags={handleVisitedFlags}
+            ></CountryData>
+        </div>
+    );
+};
+
+export default CountryDetail;
